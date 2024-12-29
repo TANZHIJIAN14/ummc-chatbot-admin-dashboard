1ec6869dd7db1fa904702b641d0c3311158a12c1
TANZHIJIAN14 <jiantanzhi@gmail.com>
2024-12-29 22:25:41 +0800

    Minor change CSS

--- css.py
+++ css.py
@@ -5,13 +5,13 @@ custom_css = """
     width: 100%;         /* Table occupies full width */
 }
 #dataframe-container th:nth-child(1), #dataframe-container td:nth-child(1) {
-    width: 150px; /* Width for the first column (_id) */
+    width: 220px; /* Width for the first column (Id) */
 }
 #dataframe-container th:nth-child(2), #dataframe-container td:nth-child(2) {
-    width: 150px; /* Width for the second column (user_id) */
+    width: 220px; /* Width for the second column (User ID) */
 }
 #dataframe-container th:nth-child(3), #dataframe-container td:nth-child(3) {
-    width: 300px; /* Width for the third column (message) */
+    width: 300px; /* Width for the third column (Message) */
     white-space: pre-wrap;  /* Allow breaking lines */
     overflow: hidden;       /* Hide overflow */
     text-overflow: ellipsis;/* Add ellipsis if needed */
@@ -19,6 +19,9 @@ custom_css = """
     word-wrap: break-word;  /* Break long words */
 }
 #dataframe-container th:nth-child(4), #dataframe-container td:nth-child(4) {
-    width: 150px; /* Width for the fourth column (created_at) */
+    width: 150px; /* Width for the fourth column (Label) */
+}
+#dataframe-container th:nth-child(5), #dataframe-container td:nth-child(5) {
+    width: 220px; /* Width for the fifth column (Created At) */
 }
 """
